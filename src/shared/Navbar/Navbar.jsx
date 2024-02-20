e1408651309dc42dd873a4e19fbea5af83990c12
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-02-20 20:54:26 +0600

    Navber done

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,9 +1,63 @@
 import React from "react";
+import logo from "../../../public/logo.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const navLink = (
+    <>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/about'>About</Link>
+      </li>
+      <li>
+        <Link>Services</Link>
+      </li>
+      <li>
+        <Link>Blog</Link>
+      </li>
+      <li>
+        <Link>Contact</Link>
+      </li>
+    </>
+  );
   return (
-    <div>
-      <h1>this is Navbar</h1>
+    <div className='navbar bg-base-100 font-semibold'>
+      <div className='navbar-start'>
+        <div className='dropdown'>
+          <div tabIndex={0} role='button' className='btn btn-ghost lg:hidden'>
+            {" "}
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M4 6h16M4 12h8m-8 6h16'
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+          >
+            {navLink}
+          </ul>
+        </div>
+        <img src={logo} alt='' />
+      </div>
+      <div className='navbar-center hidden lg:flex'>
+        <ul className='menu menu-horizontal px-1'>{navLink}</ul>
+      </div>
+      <div className='navbar-end'>
+        <button className='btn btn-outline text-[#FF3811] '>Appointment</button>
+      </div>
     </div>
   );
 };
