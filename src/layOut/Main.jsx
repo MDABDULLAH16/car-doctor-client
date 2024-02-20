6eeb6f841c71d7041d8fef37395283d7763d65f3
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-02-20 23:11:34 +0600

    home page banner done

--- src/layOut/Main.jsx
+++ src/layOut/Main.jsx
@@ -5,7 +5,7 @@ import Footer from "../shared/Footer/Footer";
 
 const Main = () => {
   return (
-    <div>
+    <div className='m-8'>
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div>
+      <Banner></Banner>
       <h1>this is Home</h1>
     </div>
   );
