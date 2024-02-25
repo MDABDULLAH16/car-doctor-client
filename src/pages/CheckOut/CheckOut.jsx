c5c34d14ec0535c3e6aa16d86a9273d2f6f340ed
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-02-25 16:08:52 +0600

    bookings done

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -1,18 +1,52 @@
 import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
+import { data } from "autoprefixer";
 
 const CheckOut = () => {
   const service = useLoaderData();
+  console.log(service);
   const { user } = useContext(AuthContext);
-  const { title, price } = service;
+  const { title, price, img, _id } = service;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const date = form.date.value;
+    const price = form.price.value;
+    const order = {
+      customerName: name,
+      email,
+      img,
+      date,
+      price,
+      service: title,
+      service_id: _id,
+    };
+    fetch("http://localhost:5000/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(order),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.insertedId) {
+          alert("bookings Successfully");
+        }
+      });
+  };
   return (
     <div>
       <h4 className='font-bold text-2xl text-center text-[#FF3811]'>
         Booking Service: {title}{" "}
       </h4>
 
-      <form className='card-body  '>
+      <form onSubmit={handleSubmit} className='card-body  '>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
           <div className='form-control'>
             <label className='label'>
@@ -71,7 +105,7 @@ const CheckOut = () => {
           <input
             className='btn btn-block btn-primary'
             type='submit'
-            value='Booking Now'
+            value='Order Confirm'
           />
         </div>
       </form>
